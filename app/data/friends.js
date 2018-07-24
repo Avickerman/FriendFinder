9e4bfa734a848d857a718fa58dcef85ae566ee6a
Avickerman <Avickerm@gmail.com>
2018-07-23 23:33:10 -0500

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+var friendsArray = [
+    {
+        name: 'Matt Peterson',
+        photo: 'https://static.turbosquid.com/Preview/2014/08/02__02_27_43/Business_Man_00000.jpgfe071256-9609-4caa-8e56-491c3636a91aRes200.jpg',
+        scores: [
+            '2',
+            '3',
+            '5',
+            '1',
+            '4',
+            '3',
+            '2',
+            '5',
+            '1',
+            '3'
+        ]
+    },
+    {
+        name: "Ahmed Aaron",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            '5',
+            '1',
+            '4',
+            '4',
+            '5',
+            '1',
+            '2',
+            '5',
+            '4',
+            '1'
+        ]
+    },
+    {
+        name: 'Ryan Anderson',
+        photo: 'https://static.turbosquid.com/Preview/001213/928/6K/3D-cartoon-man-rigged_200.jpg',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'John Smith',
+        photo: 'https://static.turbosquid.com/Preview/2014/08/02__05_46_31/green_rigRender00.jpg8fe18c55-385c-4f6b-b7d2-aef3a465b08dRes200.jpg',
+        scores: [
+            '1',
+            '2',
+            '3',
+            '3',
+            '5',
+            '1',
+            '5',
+            '1',
+            '1',
+            '2'
+        ]
+    },
+    {
+        name: 'Andy Nurphy',
+        photo: 'https://i.pinimg.com/originals/2c/31/cf/2c31cf2bcb0920013d54f7044275324a.jpg',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '5',
+            '5',
+            '3',
+            '5',
+            '5',
+            '4',
+            '5'
+        ]
+    },
+    {
+        name: 'Liz Johnson',
+        photo: 'https://static.turbosquid.com/Preview/2014/07/11__23_05_37/glassgirl_rig_render01f7f7f7.jpg2ec25481-737d-42c2-bb17-0ffe9d781937Res200.jpg',
+        scores: [
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Gina Patterson',
+        photo: 'https://thumbs.dreamstime.com/t/presentaci%C3%B3n-sonriente-de-la-mujer-de-negocios-de-la-historieta-50763663.jpg',
+        scores: [
+            '4',
+            '3',
+            '1',
+            '5',
+            '2',
+            '1',
+            '2',
+            '1',
+            '2',
+            '5'
+        ]
+    },
+    {
+        name: 'Sue Anderson',
+        photo: 'https://static.turbosquid.com/Preview/2014/05/21__12_54_12/blond_rigged_render03b.jpg2ea0a22a-e835-46d3-8dc0-c4d6ea8d84bbRes200.jpg',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Emma Stone',
+        photo: 'http://r1.ilikewallpaper.net/ipad-wallpapers/download/29375/Anime-Angelic-Xmas-Redhead-ipad-wallpaper-ilikewallpaper_com_200.jpg',
+        scores: [
+            '4',
+            '2',
+            '4',
+            '4',
+            '2',
+            '4',
+            '1',
+            '3',
+            '4',
+            '5'
+        ]
+    },
+    {
+        name: 'Cara Riely',
+        photo: 'https://static.turbosquid.com/Preview/2014/09/07__11_42_25/afro_rigRenderMR02.jpgb7c17e9e-4962-462a-ac2f-97a32f044249Res200.jpg',
+        scores: [
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '1',
+            '5',
+            '5',
+            '5'
+        ]
+    }
+
+];
+
+module.exports = friendsArray;
